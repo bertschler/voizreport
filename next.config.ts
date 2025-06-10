bdc9532cebd61237cb4972bb169d749086ac22f9
Mario Bertschler <mario@hellotracks.com>
2025-06-10 19:17:47 +0200

    asdf

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.*'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
